3e3e73126e9e53bdbce35e8607b2ae895a12a8fe
Mr.Dokepon <Gigabiz.it@gmail.com>
2017-10-07 21:37:43 +0700

    Add Router4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
+
+const HomeApp = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+    </div>
+  )
+}
+
+const NotFound = () => {
+  return (
+    <div>
+      <center><h1>404 Not Found</h1></center>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={HomeApp} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
 
-export default App;
+export default App
